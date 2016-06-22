8311d4583aabaa8ab8491ff09f411525cc837422
Taro Omiya <taro.omiya@omiyagames.com>
2016-06-21 22:46:15 -0400

    Fixing error on create_tream_role_in_submissions.rb.

--- db/migrate/20160621023709_create_team_role_in_submissions.rb
+++ db/migrate/20160621023709_create_team_role_in_submissions.rb
@@ -3,9 +3,8 @@ class CreateTeamRoleInSubmissions < ActiveRecord::Migration
     create_table :team_role_in_submissions do |t|
       t.text :role
 
-      t.belongs_to :team, index: true,
-      t.belongs_to :submission, index: true,
-
+      t.belongs_to :team, index: true
+      t.belongs_to :submission, index: true
       t.timestamps null: false
     end
   end
