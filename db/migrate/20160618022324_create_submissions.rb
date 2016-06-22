c4ee633d66d5459d71d887f0f8ddbff92c9cb79b
Taro Omiya <taro.omiya@omiyagames.com>
2016-06-21 22:54:26 -0400

    Updating field names in the migrate files.  At this point, deleting the old database is recommended.

--- db/migrate/20160618022324_create_submissions.rb
+++ db/migrate/20160618022324_create_submissions.rb
@@ -1,12 +1,10 @@
 class CreateSubmissions < ActiveRecord::Migration
   def change
     create_table :submissions do |t|
-      t.string :title
+      t.string :name
       t.text :description
       t.binary :header_image
       t.binary :icon_image
-      t.date :submitted
-      t.date :updated
 
       t.timestamps null: false
     end
