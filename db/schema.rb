b70b269d3fdda8106b8a149ac0fa3b46de3c6cce
Taro Omiya <taro.omiya@omiyagames.com>
2016-06-22 00:05:12 -0400

    Updating some migration settings, since I wasn't satisfied with the first set.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618022324) do
+ActiveRecord::Schema.define(version: 20160621031234) do
 
   create_table "events", force: :cascade do |t|
-    t.string   "title"
+    t.string   "name"
     t.text     "description"
     t.binary   "header_image"
     t.binary   "icon_image"
@@ -23,37 +23,89 @@ ActiveRecord::Schema.define(version: 20160618022324) do
     t.date     "submission_grace_period"
     t.boolean  "submission_open"
     t.integer  "status"
-    t.date     "submitted"
-    t.date     "updated"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
 
+  add_index "events", ["name"], name: "index_events_on_name"
+  add_index "events", ["start"], name: "index_events_on_start"
+
   create_table "submissions", force: :cascade do |t|
-    t.string   "title"
+    t.string   "name"
     t.text     "description"
     t.binary   "header_image"
     t.binary   "icon_image"
-    t.date     "submitted"
-    t.date     "updated"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "event_id"
   end
 
+  add_index "submissions", ["event_id"], name: "index_submissions_on_event_id"
+
+  create_table "team_role_in_submissions", force: :cascade do |t|
+    t.text     "role"
+    t.integer  "team_id"
+    t.integer  "submission_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "team_role_in_submissions", ["submission_id"], name: "index_team_role_in_submissions_on_submission_id"
+  add_index "team_role_in_submissions", ["team_id"], name: "index_team_role_in_submissions_on_team_id"
+
   create_table "teams", force: :cascade do |t|
-    t.string   "title"
+    t.string   "name"
     t.text     "description"
     t.binary   "team_image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
+  create_table "user_role_in_events", force: :cascade do |t|
+    t.text     "message"
+    t.integer  "status"
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_role_in_events", ["event_id"], name: "index_user_role_in_events_on_event_id"
+  add_index "user_role_in_events", ["user_id"], name: "index_user_role_in_events_on_user_id"
+
+  create_table "user_role_in_submissions", force: :cascade do |t|
+    t.text     "role"
+    t.integer  "status"
+    t.integer  "user_id"
+    t.integer  "submission_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "user_role_in_submissions", ["submission_id"], name: "index_user_role_in_submissions_on_submission_id"
+  add_index "user_role_in_submissions", ["user_id"], name: "index_user_role_in_submissions_on_user_id"
+
+  create_table "user_role_in_teams", force: :cascade do |t|
+    t.text     "role"
+    t.integer  "status"
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "user_role_in_teams", ["team_id"], name: "index_user_role_in_teams_on_team_id"
+  add_index "user_role_in_teams", ["user_id"], name: "index_user_role_in_teams_on_user_id"
+
   create_table "users", force: :cascade do |t|
-    t.string   "title"
+    t.string   "username"
+    t.string   "realname"
     t.text     "description"
     t.binary   "profile_image"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
